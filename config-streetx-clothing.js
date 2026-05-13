5e4e46201f83c9b367524976d941e0e5a1cd99b8
www.ranga.com <ranga2007.03.09@gmail.com>
2026-05-14 02:04:41 +0530

    Add files via upload

---
+++ config-streetx-clothing.js
@@ -0,0 +1,11 @@
+// STREETX FIREBASE CONFIGURATION
+const streetxFirebaseConfig = {
+    apiKey: "AIzaSyB_ZQ4t__Xe-v_B-rhG84JghpicDjVSAFE",
+    authDomain: "streetx-site.firebaseapp.com",
+    databaseURL: "https://streetx-site-default-rtdb.firebaseio.com", // Manually added for Realtime Database
+    projectId: "streetx-site",
+    storageBucket: "streetx-site.firebasestorage.app",
+    messagingSenderId: "921436853043",
+    appId: "1:921436853043:web:447f82245c05acba3b2446",
+    measurementId: "G-EZBKJFFLFP"
+};
